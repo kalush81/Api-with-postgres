0e37e511fd200ee5ab4b33db4535366ae1c92685
kalush81 <krzysiek.kaluza@gmail.com>
2022-01-25 09:40:05 +0100

    Send books as response to reg /

--- src/database.ts
+++ src/database.ts
@@ -17,6 +17,6 @@ const client = new Pool({
   password: POSTGRES_PASSWORD
 });
 
-client.connect();
+//client.connect();
 
 export default client
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import { BookStore } from './models/books'
+
+const bookStore = new BookStore();
 
 const app: express.Application = express()
 const address: string = "localhost:3000"
 
 app.use(bodyParser.json())
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
+app.get('/', async function (req: Request, res: Response):Promise<void> {
+    const books = await bookStore.index()
+    res.send(books)
 })
 
 app.listen(3000, function () {
