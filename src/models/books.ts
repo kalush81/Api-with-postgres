7e0a32b2fdbc72d1edbd09b0904715f3270d4045
kalush81 <krzysiek.kaluza@gmail.com>
2022-01-25 09:41:14 +0100

    Create create async func to create book and save it to postgres

--- src/models/books.ts
+++ src/models/books.ts
@@ -11,6 +11,7 @@ export type Book = {
 }
 
 export class BookStore { 
+  //get all books 
   async index(): Promise<Book[]> { 
     try {
       const conn = await client.connect()
@@ -21,6 +22,19 @@ export class BookStore {
       
     } catch (error) {
       throw new Error(`Could not get books: ${error}`)
+    }
+  }
+  async create(b: Book): Promise<Book> { 
+    try {
+      const conn = await client.connect()
+      const sql = 'INSERT INTO books (title, author, total_pages, type, summary) VALUES($1, $2, $3, $4, $5) RETURNING*'
+      const props = [b.title, b.author, b.total_pages, b.type, b.summary]
+      const result = await conn.query(sql, props)
+      conn.release();
+      return result.rows[0]
+      
+    } catch (error) {
+      throw new Error(`couldn't create book, ${b.title}, ${error}`)
     }
   }
 
