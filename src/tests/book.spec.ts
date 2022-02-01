899ac2694df90ac40afd5148779db54fd0eb777d
kalush81 <krzysiek.kaluza@gmail.com>
2022-02-01 16:19:25 +0100

    test a book by id, test delete book by id

--- src/tests/book.spec.ts
+++ src/tests/book.spec.ts
@@ -4,7 +4,8 @@ console.log(process.env.ENV);
 
 const book = new BookStore;
 
-describe('book model', () => {
+describe('drop book model', () => {
+  
   it('should hava an index method', () => {
     expect(book.index).toBeDefined;
   });
@@ -30,6 +31,27 @@ describe('book model', () => {
       type: 'Childrens',
       summary: ''
     });
+  });
+
+  it('get one book by id', async() => { 
+    book.show("1");
+    const result = await book.index()
+
+    expect(result).toEqual([{
+      id: 1,
+      title: 'Bridge to Terabithia',
+      total_pages: 250,
+      author: 'Katherine Paterson',
+      type: 'Childrens',
+      summary: ''
+    }]);
+  })
+
+  it('delete method should remove the book', async () => {
+    await book.delete("1");
+    const result = await book.index()
+
+    expect(result).toEqual([]);
   });
 
 });
